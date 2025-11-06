18869f4d7307390792aeb9e5aacb2e4d76fe1448
Vinícius Dias da Rocha <35822025+vaineoneandonly@users.noreply.github.com>
2025-11-06 20:27:46 +0000

    started hangman!

---
+++ hangman.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main()
+{
+    std::string a {"sdajkdskad"};
+    std::string r;
+    r.append(a.length(), '_');
+
+    int i {static_cast<int>(a.find("k"))};
+
+    std::vector<int> idxs {1, 2, 3};
+    
+    for (int i : idxs) 
+    {
+        std::cout << i << '\n';
+        r.replace(i, 2, "o");
+    }
+    std::cout << r << '\n';    
+}
