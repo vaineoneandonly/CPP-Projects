d7407de3a8bd79ad7815db5d573dce10e154b634
Vinícius Dias da Rocha <35822025+vaineoneandonly@users.noreply.github.com>
2025-11-10 12:08:59 +0000

    secon version of hangman!

--- hangman.cpp
+++ hangman.cpp
@@ -4,19 +4,41 @@
 
 int main()
 {
-    std::string a {"sdajkdskad"};
-    std::string r;
-    r.append(a.length(), '_');
+    std::string word {"sdajkdskad"};
+    std::string spaces;
+    spaces.append(word.length(), '_');
 
-    int i {static_cast<int>(a.find("k"))};
-
-    std::vector<int> idxs {1, 2, 3};
+    std::string guess;
     
-    for (int i : idxs) 
+    int limbs {6};
+    
+    system("clear");
+    while (limbs != 0)
     {
-        std::cout << i << '\n';
-        r.replace(i, 2, "o");
+        std::cout << spaces << '\n';
+
+        std::cout << "guess a letter: ";
+        std::cin >> guess;
+
+        int cidx {0};
+        for (char c : word) 
+        {
+            std::string currentChar {c};
+            if (guess == currentChar)
+            {
+                std::cout << "found! \n";
+                spaces.replace(cidx, 1, currentChar);
+            }
+            else
+            {
+                --limbs;
+            }
+            ++cidx;
+        }
+
+        std::cout << "\nany button to continue. ";
+        std::cin >> guess;
+
+        system("clear");
     }
-    std::cout << r << '\n';    
-    std::cout << "now?";
 }
