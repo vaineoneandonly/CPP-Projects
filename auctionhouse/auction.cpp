59e474ae4c59c9fbe93323384bd9b05593733fd0
Vinícius Dias da Rocha <35822025+vaineoneandonly@users.noreply.github.com>
2025-11-24 14:19:08 +0000

    auction house system working!

--- auctionhouse/auction.cpp
+++ auctionhouse/auction.cpp
@@ -15,21 +15,35 @@ struct bid
 {
     std::string   bidder;
     unsigned int ammount;
+
 };
 std::ostream &operator<<(std::ostream &os, const bid &b)
 {
     os << b.ammount << ", bid by " << b.bidder;
     return os;
 }
+std::istream &operator>>(std::istream &in, bid &b)
+{
+    std::cout << "who goes there and how much are they willing to pay?\n";
+    in >> b.bidder; 
+    in >> b.ammount;
+
+    return in;
+}
 
 struct auction
 {
     item auctioned;
     bid highest;
+    unsigned int rounds = 3;
 
-    auction &operator<=>(const bid &current)
+    void placeBid(const bid &newBid)
     {
-        return highest <=> current;
+        if (newBid.ammount > highest.ammount)
+        {
+            std::cout << newBid << ", takes the forefront!\n";
+            highest = newBid;
+        }
     }
 };
 std::ostream &operator<<(std::ostream &os, const auction &a)
@@ -38,20 +52,38 @@ std::ostream &operator<<(std::ostream &os, const auction &a)
     return os;
 }
 
+
 int main()
 {
     item i1{"an old lady", 2000};
     bid current{"nobody", 1};
     auction a1{i1, current};
 
-    std::cout << a1 << "\n";
-
     current = {"the gentleman with the white shirt in the back", 3};
+        
+    std::cout << a1 << '\n';
 
-    if (current.ammount > a1.highest.ammount)
+    unsigned int control = 0;
+    while (a1.rounds > 0)
     {
-        a1.highest = current;
+        std::cout << "1 for placing a new bet on " << a1.auctioned << ", currently at " << a1.highest.ammount << ", or anything else to move on!";
+        std::cin >> control;
+
+        switch (control)
+        {
+            case 1:
+                std::cin >> current;
+                a1.placeBid(current);
+                break;
+        
+            default:
+                std::cout << "no new bids this round!\n";
+                break;
+        }
+
+        --a1.rounds;
+        std::cout << "onwards and outwards to round " << a1.rounds << " of the auction!\n";
     }
 
-    std::cout << a1 << '\n';
+    std::cout << a1.auctioned << " won by " << a1.highest.bidder << ", at " << a1.highest.ammount;
 }
