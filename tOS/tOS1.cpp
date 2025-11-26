34a9d7ea5a16c0761f4a7b0ccb9851e8b544c56f
Vinícius Dias da Rocha <35822025+vaineoneandonly@users.noreply.github.com>
2025-11-26 13:40:25 +0000

    portuguese sentence identifier begginings!

---
+++ tOS/tOS1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+enum Conjugacao
+{
+    Eu,
+    Tu,
+    Ele,
+    Nos,
+    Vos,
+    Eles,
+};
+
+int main()
+{
+    const std::string Terminacoes[3] {"ar", "er", "ir"}; 
+    const std::string verbos[3] {"rezar", "andar", "aturdir"};
+
+    const std::string   PresenteIndicativo[3][6]
+                        {
+                            {"o", "as", "a", "amos", "ais", "am"},
+                            {"o", "es", "e", "emos", "eis", "em"},
+                            {"o", "es", "e", "emos", "eis", "em"},
+                        };
+
+
+    std::string verboAtual {"correr"};
+    
+    std::string terminaCom {verboAtual.substr( verboAtual.length()-2, verboAtual.length()-1)};
+    int c//ategoria
+    {
+        terminaCom == "ar" ? 0 : 
+        terminaCom == "er" ? 1 :
+        terminaCom == "ir" ? 2 : 
+        3
+    };
+
+    for (int i = 0; i < 6; ++i)
+    {
+        std::cout << verboAtual.substr(0, verboAtual.length()-2) << PresenteIndicativo[c][i] << '\n';
+    }
+}
