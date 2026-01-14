f25204e10956a546ed99a48552d880f09ee8e287
Vinícius Dias da Rocha <35822025+vaineoneandonly@users.noreply.github.com>
2026-01-14 21:00:20 +0000

    some changes to autoitems.

--- SL/autoitems.cpp
+++ SL/autoitems.cpp
@@ -1,40 +1,70 @@
 #include <iostream>
 #include <fstream>
 
-int main()
+void paraVOLVO()
 {
-    std::string modelo       {"I 656-7446 00                                            PC 6567446#00                   962 "};
-    std::string modeloDAFMAG {"I ATRC292                                                PC ATRC292#VV                   962 "};
+    std::string modelo {"I xx-yyyy vv                                              PC xxyyyy#vv                    "};
+    std::string prodVOLVO {"6884427	FITA                                              "};
+    
+    int i {static_cast<int>(prodVOLVO.find_first_of('	'))};
 
-    std::ifstream products{"prodAndVerDAFMAGIUSFILIAL"}; // <<<
-    std::string prod {"6138152	90"};
+    std::string prod {prodVOLVO.substr(0, i)};
 
-    std::ofstream newProds{"newProdsDAFMAG"};
+    std::cout << prod << '\n';
+}
+
+void paraCAT()
+{
+ std::string modelo       {"I xx-yyyy vv                                              PC xxyyyy#vv                    "};
+    
+    std::ifstream products{"modeloProdutosVEXSP2"}; // <<<
+    std::string prod {"1P-2807 07                     	-                                                 "};
+
+    std::ofstream newProds{"novosProdutosVEXSP2"};
     
     while (std::getline(products, prod))
     {
-        //std::string prodStart {prod.substr(0, 3)};
-        //std::string prodEnd   {prod.substr(3, 4)};
+        std::string prodStart        {prod.substr(0, 2)};
+        std::string prodEnd          {prod.substr(3, 4)};
+        std::string prodVer          {prod.substr(8, 2)};
+        std::string prodDesc         {prod.substr(32, 35)};
+
+        /*
+        std::cout << prodStart << '-' 
+                  << prodEnd << ' ' 
+                  << prodVer << " description:" 
+                  << prodDesc << '\n';
+        */
+
+        modelo.replace(2, 2, prodStart);
+        modelo.replace(5, 4, prodEnd);
+        modelo.replace(10, 2, prodVer);
+        modelo.replace(22, 35, prodDesc);
+
+        modelo.replace(61, 2, prodStart);
+        modelo.replace(63, 4, prodEnd);
+        modelo.replace(68, 2, prodVer);
+
+        newProds << modelo << '\n';
         
-        std::string justProd {prod.substr(0, 7)};
-        std::string prodVer   {prod.substr(8, 2)};
+        //modeloVEX.replace(2, 7, justProd);
+        //modeloVEX.replace(60, 7, justProd);
 
-
-
-        //std::cout << prod.substr(0, 7) << ' ' << prod.substr(8, 2) << '\n';
-
-        modeloDAFMAG.replace(2, 7, justProd);
-        modeloDAFMAG.replace(60, 7, justProd);
-
-        //modeloDAFMAG.replace(11, 2, prodVer);
-        modeloDAFMAG.replace(68, 2, prodVer);
+        //modeloVEX.replace(11, 2, prodVer);
+        //modeloVEX.replace(68, 2, prodVer);
         
-        newProds << modeloDAFMAG << '\n';
+        //newProds << modeloVEX << '\n';
 
 
-        //qqmodeloDAFMAG.replace(63, 4, prodEnd);
+        //qqmodeloVEX.replace(63, 4, prodEnd);
 
-        //std::cout << modeloDAFMAG << '\n';
-        //newProds << modeloDAFMAG << '\n';
+        //std::cout << modeloVEX << '\n';
+        //newProds << modeloVEX << '\n';
     }
+}
+
+
+int main()
+{
+    paraVOLVO();
 }
